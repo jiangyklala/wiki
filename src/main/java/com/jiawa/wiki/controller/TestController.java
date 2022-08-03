1b410691460b6733f5dc340dccdd3050e87bb88a
Jxm <969904729@qq.com>
2022-08-03 17:43:13 +0800

    3-5 集成mybatis generator, 生成demo表代码

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,13 +1,27 @@
 package com.jiawa.wiki.controller;
 
+import com.jiawa.wiki.domain.Test;
+import com.jiawa.wiki.service.TestService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class TestController {
 
+    @Resource
+    private TestService testService;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!!";
+    }
+
+    @GetMapping("/test/list")
+    public List<Test> list() {
+        return testService.list();
     }
 }

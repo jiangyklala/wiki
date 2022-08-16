0d4434f426c1141cb5346a5f0e80eab3e98533d0
Jxm <969904729@qq.com>
2022-08-16 11:48:00 +0800

    6-11 继承 Validation, 为列表参数增加参数校验

--- src/main/java/com/jiawa/wiki/req/PageReq.java
+++ src/main/java/com/jiawa/wiki/req/PageReq.java
@@ -1,9 +1,16 @@
 package com.jiawa.wiki.req;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
 public class PageReq {
 
+    @NotNull(message = "[每页参数] 不能为空")
     private int page;
 
+
+    @NotNull(message = "[每页参数] 不能为空")
+    @Max(value = 100, message = "[每页参数] 不能超过100")
     private int size;
 
     public int getPage() {
--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -66,6 +66,7 @@
 <script lang="ts">
 import {defineComponent, onMounted, ref} from 'vue';
 import axios from 'axios';
+import { message } from 'ant-design-vue';
 
 export default defineComponent({
   name: 'AdminEbook',
@@ -73,7 +74,7 @@ export default defineComponent({
     const ebooks = ref();
     const pagination = ref({
       current: 1,
-      pageSize: 4,
+      pageSize: 101,
       total: 0
     });
     const loading = ref(false);
@@ -131,11 +132,15 @@ export default defineComponent({
       }).then((response) => {
         loading.value = false;
         const data = response.data;
-        ebooks.value = data.content.list;
+        if (data.success) {
+          ebooks.value = data.content.list;
 
-        // 重置分页按钮
-        pagination.value.current = params.page;
-        pagination.value.total = data.content.total;
+          // 重置分页按钮
+          pagination.value.current = params.page;
+          pagination.value.total = data.content.total;
+        } else {
+          message.error(data.message);
+        }
       });
     };
 

df91473ecab82f3597a5ea66f7b65787c3bbc1fd
Jxm <969904729@qq.com>
2022-08-20 15:19:35 +0800

    7-4 分类管理: 不显示分页

--- src/main/java/com/jiawa/wiki/service/CategoryService.java
+++ src/main/java/com/jiawa/wiki/service/CategoryService.java
@@ -30,8 +30,20 @@ public class CategoryService {
     @Resource
     private SnowFlake snowFlake;
 
+    public List<CategoryQueryResp> all() {
+        CategoryExample categoryExample = new CategoryExample();
+        categoryExample.setOrderByClause("sort asc");
+        List<Category> categoryList = categoryMapper.selectByExample(categoryExample);
+
+        // 列表复制
+        List<CategoryQueryResp> list = CopyUtil.copyList(categoryList, CategoryQueryResp.class);  // 列表复制
+
+        return list;
+    }
+
     public PageResp<CategoryQueryResp> list(CategoryQueryReq req) {
         CategoryExample categoryExample = new CategoryExample();
+        categoryExample.setOrderByClause("sort asc");
         CategoryExample.Criteria criteria = categoryExample.createCriteria();
         PageHelper.startPage(req.getPage(), req.getSize());
         List<Category> categoryList = categoryMapper.selectByExample(categoryExample);
@@ -59,6 +71,7 @@ public class CategoryService {
 
         return pageResp;
     }
+
 
     /**
      * 保存

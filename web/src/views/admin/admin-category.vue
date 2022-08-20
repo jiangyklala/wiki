f467389e5bcd5e78451da90ac08bd30d79fb769a
Jxm <969904729@qq.com>
2022-08-20 21:29:16 +0800

    7-5 添加分类时改为下拉框选择

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -21,7 +21,7 @@
       <a-table
           :columns="columns"
           :row-key="record => record.id"
-          :data-source="categorys"
+          :data-source="level1"
           :loading="Loading"
           :pagination="false"
       >
@@ -60,7 +60,17 @@
         <a-input v-model:value="category.name" />
       </a-form-item>
       <a-form-item label="父分类">
-        <a-input v-model:value="category.parent" />
+        <a-select
+            v-model:value="category.parent"
+            ref="select"
+        >
+          <a-select-option value="0">
+            无
+          </a-select-option>
+          <a-select-option v-for="c in level1" :key="c.id" :value="c.id" :disabled="category.id == c.id">
+            {{ c.name }}
+          </a-select-option>
+        </a-select>
       </a-form-item>
       <a-form-item label="顺序">
         <a-input v-model:value="category.sort" />
@@ -106,6 +116,18 @@ export default defineComponent({
     ];
 
     /**
+     * 一级分类树, children属性就是二级分类
+     * [{
+     *  id: "",
+     *  children: [{
+     *    id: "",
+     *    name: "",
+     *  }]
+     * }]
+     */
+    const level1 = ref(); // 一级分类树, children属性就是二级分类
+
+    /**
      * 数据查询
      */
     const handleQuery = () => {
@@ -115,6 +137,11 @@ export default defineComponent({
         const data = response.data;
         if (data.success) {
           categorys.value = data.content;
+          console.log("原始数组: ", category.value);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys.value, 0);
+          console.log("树形结构: ", level1);
         } else {
           message.error(data.message);
         }
@@ -177,7 +204,8 @@ export default defineComponent({
 
     return {
       param,
-      categorys,
+      // categorys,
+      level1,
       columns,
       loading,
       handleQuery,
